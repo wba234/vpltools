2aebfa8f095b257e489fcc47ffaf638964feccf9
William Bailey <william.bailey@centre.edu>
2024-09-10 10:59:03 -0400

    Added a class specifically for searching through command history files submitted for grading.

--- src/findmodules/__init__.py
+++ src/findmodules/__init__.py
@@ -2,4 +2,5 @@ from findmodules.findmodules import *
 from findmodules.make_vpl_evaluate_cases import *
 from findmodules.vpl_test_case import VPLTestCase, VPLProgramTester, SemanticCaseType
 from findmodules.opaquetest import *
+from historysearcher import *
 # from findmodules.vpl_unittest import *
