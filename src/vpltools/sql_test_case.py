8d1534874d8264d3d74e10cd68b66d57bf984a5b
William Bailey <wba234@g.uky.edu>
2025-06-23 14:35:08 -0400

    Added MariaDB as a backend option.

--- src/vpltools/sql_test_case.py
+++ src/vpltools/sql_test_case.py
@@ -1,71 +1,160 @@
-from typing import Type
-# import mariadb
-import unittest
+# from typing import Type
+import re
+import sys
+import abc
 import os.path
+import subprocess
+import unittest
 import sqlite3 as sl
-from itertools import permutations
-from enum import Enum
+
+import mariadb
 import pandas as pd
+from enum import Enum
+from itertools import permutations
+
 import vpltools
 
 __unittest = True
-class InMemoryTestingDatabase:
-    '''
-    An object representing an ephemeral SQLite 3 database.
-    setup_script_name : string representing an SQL script which builds a database.
-    '''
-    def __init__(self, setup_script_name: str):
-        self.connection = sl.connect(":memory:")
-        with open(setup_script_name, 'r') as fo:
-            script_contents = fo.read()
-            
-        self.cursor = self.connection.executescript(script_contents)
-
-
-    def getConnection(self) -> sl.Connection:
-        return self.connection
 
 class SupportedSQLBackends(Enum):
     MariaDB = 'mariadb'
     SQLite3 = 'sqlite3'
 
-# class TestSQLQuery(unittest.TestCase): #, abc.ABC):
+
+
+class Database(abc.ABC):
+    '''
+    An abstract base class representing a database which can be queried.
+    Subclasses of this class require:
+    1. a class attribute 'conn' to be defined. This should be an 
+       _SQLConnection object, ideally, of the type supported by Pandas.
+    2. a constructor accepting a the name of a script which will 
+       be used to setup and populate the test database.
+    2. a run_query() method. This should accepts a single string 
+       of SQL code to be executed. The overridden method can use 
+       the super() implementation, which uses Pandas' read_sql_query().
+    '''
+    conn = None 
+    
+    @abc.abstractmethod
+    def __init__(self):
+        raise NotImplementedError
+    
+
+    @abc.abstractmethod
+    def run_query(self, query: str) -> pd.DataFrame:
+        return pd.read_sql_query(query, self.conn)
+    
+
+
+class MariaDBPersistentDatabase(Database):
+    '''
+    An object representing a persistent MariaDB database.
+    setup_script_name : string representing an SQL script 
+    which builds a database.
+    '''
+    def __init__(self, setup_script_name: str, user: str, password: str, db_name: str):
+        '''
+        Sets up a MariaDB database by running setup_script_name, using
+        the provided credentials. Note that the name of the database
+        created by the script must match the provided db_name exactly.
+        '''
+        # The MariaDB connector does not support running scripts. 
+        # So we run it in it's own process, command-line style.
+        s = subprocess.run(["/usr/bin/mariadb", f"-u{user}", f"-p{password}", "-e", f"SOURCE {setup_script_name}"])
+
+        if s.returncode != 0:
+            raise RuntimeError("Couldn't initialize MariaDB database.\nPlease send the complete "
+                               + "traceback from this error message to your instructor.")
+        
+        try:
+            self.conn = mariadb.connect(
+                user=user,
+                password=password,
+                host="localhost",
+                database=db_name
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB: {e}")
+            sys.exit(1)
+
+        self.cursor = self.conn.cursor()
+
+
+    def run_query(self, query: str) -> pd.DataFrame:
+        '''
+        Executes query in the database represented by the calling object.
+        '''
+        return super().run_query(query)
+
+
+
+class InMemoryTestingDatabase(Database):
+    '''
+    An object representing an ephemeral SQLite 3 database.
+    setup_script_name : string representing an SQL script which builds a database.
+    '''
+    def __init__(self, setup_script_name: str):
+        self.conn = sl.connect(":memory:")
+        with open(setup_script_name, 'r') as fo:
+            script_contents = fo.read()
+            
+        self.cursor = self.conn.executescript(script_contents)
+
+
+    def run_query(self, query: str) -> pd.DataFrame:
+        '''
+        Executes query in the database represented by the calling object.
+        '''
+        return super().run_query(query)
+
+
+
 class TestSQLQuery(vpltools.VPLTestCase):
     '''
     A TestCase class meant to be subclassed. Provides the setup and teardown
     methods which open close connections to the database.
+
+    Users overriding this class, i.e., actually writing test for student code, should 
+    give these values in their subclasses:
+    
+    - use_database: str
+    - backend: SupportedSQLBackends
     '''
-
-    # Users overriding this class, i.e., actually writing test for student code, should 
-    # give these values in their subclasses.
-    use_database: Type[InMemoryTestingDatabase] | None = None
-
+    use_database: str = ""
     backend: SupportedSQLBackends = SupportedSQLBackends.SQLite3
 
-    # conn : mariadb.Connection = None
     conn : sl.Connection
+    db : Database 
+    db_name : str
+    db_user : str
+    db_password : str
+    # Greater abstraction may be required if multiple backends are to be supported, e.g.:
+    # conn : mariadb.Connection = None
 
-    # Greater abstraction may be required if multiple backends are to be supported.
-    # For the moment, 
     @classmethod
-    def setUpClass(cls): # type: ignore
+    def setUpClass(cls):
         '''
         Called before any test in the class is run. Sets the self.conn attribute to 
         a valid database connection, ready for queries to be submitted.
         '''
-        if cls.use_database is None:
-            raise ValueError("Cannot execute query - no database specified!")
+        super().setUpClass()
+        
+        use_database_path = os.path.join(cls.THIS_DIR_NAME, cls.use_database)
+        
+        if not cls.use_database:
+            raise ValueError(f"Cannot execute query - no database specified! Define class attribute '{cls.__name__}.use_database'")
 
         if cls.backend == SupportedSQLBackends.SQLite3:
-            rrc_db = cls.use_database() # type: ignore
-            conn = rrc_db.getConnection()
-            cls.conn = conn
+            cls.db = InMemoryTestingDatabase(use_database_path)
+            cls.conn = cls.db.conn
+
         elif cls.backend == SupportedSQLBackends.MariaDB:
-            raise NotImplementedError
+            cls.db = MariaDBPersistentDatabase(use_database_path, cls.db_user, cls.db_password, cls.db_name)
+            cls.conn = cls.db.conn
+            
         else:
             raise ValueError(f"RDBMS backend '{cls.backend}' is not supported. Choose between {list(SupportedSQLBackends)}.")
-
-        super().setUpClass()
 
 
 
@@ -76,6 +165,8 @@ class TestSQLSelectQuery(TestSQLQuery):
     out of order, and allows matching of aliased columns by ensuring 
     that all the data in each column is the same.
     '''
+    permit_select_all = True
+
     @staticmethod
     def inexplicablyNonstandardEquals(df1: pd.DataFrame, df2: pd.DataFrame) -> bool:
         '''
@@ -87,26 +178,40 @@ class TestSQLSelectQuery(TestSQLQuery):
         return all((df1 == df2).all())
 
 
-    def compareQueries(self, key_file_name: str, lab_file_name: str, file_dir: str, record_order_does_matter: bool = False) -> None:
+    def testNoSelectAll(self):
+        if self.permit_select_all:
+            return
+        
+        select_all_pattern = r"select\s+\*"
+        select_all_re = re.compile(select_all_pattern, re.IGNORECASE)
+        matches = select_all_re.findall(self.key_program_name)
+
+        self.assertListEqual(
+            matches,
+            [],
+            msg="\n'SELECT *' is not allowed here. Make sure that it doesn't appear anywhere in your submission file, even in comments. "
+        )
+
+
+    def compareQueries(self, key_file_name: str, lab_file_name: str, record_order_does_matter: bool = False) -> None:
         '''
         Helper function for testing the output of SELECT queries. 
         
-        Raises AssertionError
-        if the data sets returned by the queries in file_dir/key_file_name and 
-        file_dir/lab_file_name are not identical. The notion of identical permits record
-        order to differ. Pass record_order_does_matter=True to change this behavior.
+        Raises AssertionError if the data sets returned by the queries in 
+        key_file_name and lab_file_name are not identical. 
+        Our notion of "identical" allows record order to differ. 
+        Pass record_order_does_matter=True to change this behavior.
         The order and names of columns never matters.
         '''
-        # TODO: This could use some formal test cases.
-        key_file_path = os.path.join(file_dir, key_file_name)
-        lab_file_path = os.path.join(file_dir, lab_file_name)
+        key_file_path = os.path.join(self.THIS_DIR_NAME, key_file_name)
+        lab_file_path = os.path.join(self.THIS_DIR_NAME, lab_file_name)
 
         # Read the contents of files which contain queries.
         with open(key_file_path, 'r') as key_fo:
-            key_df = pd.read_sql_query(key_fo.read(), self.conn)
+            key_df = self.db.run_query(key_fo.read())
 
         with open(lab_file_path, 'r') as lab_fo:
-            lab_df = pd.read_sql_query(lab_fo.read(), self.conn)
+            lab_df = self.db.run_query(lab_fo.read())
 
         # Do we have the correct shape?
         self.assertEqual(
@@ -130,7 +235,7 @@ class TestSQLSelectQuery(TestSQLQuery):
             key_resolution_candidate = key_resolution_candidate.reset_index(drop=True)
         
 
-        # BTW: This can't be rolled into the loop below, because if all the 
+        # NOTE: The if below can't be rolled into the loop below it, because if all the 
         # columns match, then there are 0 permutations, and the loop doesn't 
         # run. So we need to check it here.
         if len(unmatched_key_columns) == 0 and len(unmatched_lab_columns) == 0:
@@ -139,10 +244,12 @@ class TestSQLSelectQuery(TestSQLQuery):
                 lab_resolution_candidate = lab_resolution_candidate.sort_values(list(lab_resolution_candidate.columns))
                 lab_resolution_candidate = lab_resolution_candidate.reset_index(drop=True)
 
-                self.inexplicablyNonstandardEquals(key_resolution_candidate, lab_resolution_candidate)
-
-                msg=(f"The columns were correct, but the data was not.\n"
+                self.assertTrue(
+                    self.inexplicablyNonstandardEquals(key_resolution_candidate, lab_resolution_candidate),
+                    msg=(f"The columns were correct, but the data was not.\n"
                     +f"Expected:\n{key_df}\n\nReceived:\n{lab_df}")
+                )
+
                 return  # Could keep going, but no need. Test passes.
             
 
