d154095209eeb66d1ccb3866a646adf893c96818
William Bailey <wba234@g.uky.edu>
2025-06-27 11:28:01 -0400

    Moved supported language code into own module.

--- src/vpltools/__init__.py
+++ src/vpltools/__init__.py
@@ -1,6 +1,7 @@
 from vpltools.basic_tests import *
 from vpltools.make_vpl_evaluate_cases import make_cases_file_from_list
-from vpltools.vpl_test_case import (VPLTestCase, SupportedLanguages)
+from vpltools.supported_languages import SupportedLanguages
+from vpltools.vpl_test_case import VPLTestCase
 from vpltools.sql_test_case import (TestSQLSelectQuery, InMemoryTestingDatabase, SupportedSQLBackends)
 from vpltools.historysearcher import HistorySearcher
 from vpltools.regextest import RegexTestCase
