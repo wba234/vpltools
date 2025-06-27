d0ea61300f024b66048dc336fefa7c3446742b2d
William Bailey <wba234@g.uky.edu>
2025-06-27 11:09:26 -0400

    Will parentheses fix it?

--- src/vpltools/__init__.py
+++ src/vpltools/__init__.py
@@ -1,6 +1,6 @@
 from vpltools.basic_tests import *
 from vpltools.make_vpl_evaluate_cases import make_cases_file_from_list
-from vpltools.vpl_test_case import VPLTestCase, SupportedLanguages
-from vpltools.sql_test_case import TestSQLSelectQuery, InMemoryTestingDatabase, SupportedSQLBackends
+from vpltools.vpl_test_case import (VPLTestCase, SupportedLanguages)
+from vpltools.sql_test_case import (TestSQLSelectQuery, InMemoryTestingDatabase, SupportedSQLBackends)
 from vpltools.historysearcher import HistorySearcher
 from vpltools.regextest import RegexTestCase
