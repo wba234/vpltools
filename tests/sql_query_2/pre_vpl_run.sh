97e9e7d880041ca6f440a1066eb6f3750548756b
William Bailey <william.bailey@centre.edu>
2026-02-24 14:34:06 -0500

    Updated tests to use new pre_vpl_run.sh. They don't need it.

--- tests/sql_query_2/pre_vpl_run.sh
+++ tests/sql_query_2/pre_vpl_run.sh
@@ -1,4 +1,14 @@
-python3 -m vpltoolsfor file in "key_max_developer_grades.sql"
+#!/usr/bin/env bash
+
+# Get the directory in which the present script is located.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Source - https://stackoverflow.com/a/246128
+# Posted by dogbane, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-24, License - CC BY-SA 4.0
+
+echo "$SCRIPT_DIR"
+python3 -m vpltools "$SCRIPT_DIR" &> /dev/null
+for file in "key_max_developer_grades.sql"
 do
 if [ -f "$file" ]; then
     mv "$file" "$file.save"
